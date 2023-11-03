529e3f29cdffbdcc25bb248d3818ef9b3a9062f0
Russell Vinegar <russell.vinegar@gov.bc.ca>
2023-11-03 14:35:46 -0700

    remove query  params

--- src/lambda/getapi.js
+++ src/lambda/getapi.js
@@ -2,13 +2,11 @@ const axios = require("axios")
 const qs = require("qs")
 
 export function handler(event, context, callback) {
-  // apply our function to the queryStringParameters and assign it to a variable
-  const API_PARAMS = qs.stringify(event.queryStringParameters)
   // Get env var values defined in our Netlify site UI
   const { API_TOKEN, API_URL } = process.env
   // In this example, the API Key needs to be passed in the params with a key of key.
   // We're assuming that the ApiParams var will contain the initial ?
-  const URL = `${API_URL}?${API_PARAMS}&apiKey=${API_TOKEN}`
+  const URL = `${API_URL}?&apiKey=${API_TOKEN}`
 
   // Let's log some stuff we already have.
   console.log("Injecting token to", API_URL);
